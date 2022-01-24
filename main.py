2c41dc43c653d3f327a6daa97c822554baa5f62b
Mariavellous <mm.alcaide38@gmail.com>
2022-01-23 20:37:40 -0800

    Update button working

--- main.py
+++ main.py
@@ -6,14 +6,58 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
 
+# Create a new SQLlite database with SQLAlchemy
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chick-flick-collection.db'
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 Bootstrap(app)
+
+
+#create a table in this database called "movie"
+class Movie(db.Model):
+    # These are the different fields with respective limitations
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(1000), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    ranking = db.Column(db.Integer, nullable=False)
+    review = db.Column(db.Integer, nullable=False)
+    img_url = db.Column(db.String, nullable=False)
+
+db.create_all()
+
+new_movie = Movie(
+    title="Phone Booth",
+    year=2002,
+    description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+    rating=7.3,
+    ranking=10,
+    review="My favourite character was the caller.",
+    img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+)
+# db.session.add(new_movie)
+# db.session.commit()
+
+class MovieForm(FlaskForm):
+    movie_rating = StringField(label='Your Rating Out of 10 e.g. 6.9', validators=[DataRequired()])
+    movie_review = StringField(label='Your Review', validators=[DataRequired()])
+    submit = SubmitField(label='Done')
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    movies = Movie.query.all()
+    print(movies)
+    return render_template("index.html", movies=movies)
+
+
+@app.route("/edit", methods=['GET', 'POST'])
+def edit():
+    movie_form = MovieForm()
+    return render_template("edit.html", movie_form=movie_form)
 
 
 if __name__ == '__main__':
